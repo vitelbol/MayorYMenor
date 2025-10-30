95c25dde46af8f1df46d6d8cdb9d9ded5a27d7ca
jose villegas <josevillegas13600@gmail.com>
2025-10-30 19:01:01 -0400

    Agregar archivos de proyecto.

---
+++ MayorYMenor/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MayorYMenor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNumero1 = new TextBox();
+            txtNumero2 = new TextBox();
+            txtNumero3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnCalcular = new Button();
+            SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            txtNumero1.Location = new Point(169, 59);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            txtNumero2.Location = new Point(169, 100);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 1;
+            // 
+            // txtNumero3
+            // 
+            txtNumero3.Location = new Point(169, 144);
+            txtNumero3.Name = "txtNumero3";
+            txtNumero3.Size = new Size(100, 23);
+            txtNumero3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Primer numero:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Segundo numero:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(42, 144);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Tercer numero:";
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(169, 196);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.TabIndex = 6;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(328, 261);
+            Controls.Add(btnCalcular);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNumero3);
+            Controls.Add(txtNumero2);
+            Controls.Add(txtNumero1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNumero1;
+        private TextBox txtNumero2;
+        private TextBox txtNumero3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnCalcular;
+    }
+}
---
+++ MayorYMenor/Form1.cs
@@ -0,0 +1,39 @@
+namespace MayorYMenor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int numero1 = Int32.Parse(txtNumero1.Text);
+            int numero2 = Int32.Parse(txtNumero2.Text);
+            int numero3 = Int32.Parse(txtNumero3.Text);
+
+            int mayor = numero1;
+
+            if(numero2>mayor)
+            {
+                mayor = numero2;
+            }
+            if (numero3 > mayor)
+            {
+                mayor = numero3;
+            }
+            MessageBox.Show("El numero mas grande es: " + mayor);
+            int menor = numero1;
+            if (numero2 < menor)
+            {
+                menor = numero2;
+            }
+            if (numero3 < menor)
+            {
+                menor = numero3;
+            }
+            MessageBox.Show("El numero mas pequeño es: " + menor);
+        }
+    }
+}
